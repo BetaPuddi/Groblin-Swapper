7450d72abf568e7a513309060f175a42de2315f2
Reece <18008512+BetaPuddi@users.noreply.github.com>
2025-09-17 16:03:44 +0100

    Skill script draft

---
+++ Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Skills
+{
+    public class Skill : MonoBehaviour
+    {
+        public string skillName;
+
+        public virtual void UseSkill();
+
+        public string GetUser()
+        {
+            if ()
+            {
+
+            }
+        }
+    }
+}
